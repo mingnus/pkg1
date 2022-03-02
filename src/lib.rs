7fc39b406030e979d300556c670b8b9c33541eed
Ming-Hung Tsai <mingnus@gmail.com>
2022-03-02 22:25:23 +0800

    Add test code

--- src/lib.rs
+++ src/lib.rs
@@ -17,3 +17,28 @@ impl MetadataVisitor for XmlWriter {
 fn test1() {
     pkg2::foo();
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    struct TestWriter {
+    }
+    impl MetadataVisitor for TestWriter {
+        fn superblock_b(&self) {
+        }
+    }
+
+    fn pass_metadata_visitor(v: &dyn MetadataVisitor) {
+        v.superblock_b();
+    }
+
+    fn test2(g: &dyn pkg2::MetadataGenerator) {
+        let w = TestWriter {};
+        w.superblock_b(); // ok
+        pass_metadata_visitor(&w); // ok
+
+        g.generate_metadata(&w); // failed
+        pkg2::accept_metadata_visitor(&w); // failed
+    }
+}
